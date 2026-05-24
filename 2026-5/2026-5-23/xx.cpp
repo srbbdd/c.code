1e5947f0b88e51707526809f4d78caa0b3a243cb
srbbdd <1498929267@qq.com>
2026-05-24 19:22:57 +0800

    学习

--- 2026-5/2026-5-23/xx.cpp
+++ 2026-5/2026-5-23/xx.cpp
@@ -1,42 +1,91 @@
 #include <iostream>
+#include <string>
 using namespace std;
 class Cpu
 {
 public:
 	virtual void calculat() = 0;
+	string* name;
+	virtual ~Cpu()
+	{
+		;
+	}
 };
 class VideoCard
 {
 public:
 	virtual void display() = 0;
+	string* name;
+	virtual ~VideoCard()
+	{
+		;
+	}
 };
 class Ram
 {
 public:
 	virtual void storage() = 0;
+	string* name;
+	virtual ~Ram()
+	{
+		;
+	}
 };
 class Intel:public Cpu
 {
 public:
+	Intel(string name)
+	{
+		this->name = new string(name);
+	}
 	void calculat()
 	{
-		cout << "Intel\n";
+		cout <<*name<< "Intel\n";
+	}
+	~Intel()
+	{
+		if (name != NULL)
+		{
+			delete name;
+		}
 	}
 };
 class Ram1 :public Ram
 {
 public:
+	Ram1(string name)
+	{
+		this->name = new string(name);
+	}
 	void storage()
 	{
-		cout << "Ram1\n";
+		cout <<*name<< "Ram1\n";
+	}
+	~Ram1()
+	{
+		if (name != NULL)
+		{
+			delete name;
+		}
 	}
 };
 class VideoCard1 :public VideoCard
 {
 public:
+	VideoCard1(string name)
+	{
+		this->name = new string(name);
+	}
 	void display()
 	{
-		cout << "VideoCard1\n";
+		cout << *name<<"VideoCard1\n";
+	}
+	~VideoCard1()
+	{
+		if (name != NULL)
+		{
+			delete name;
+		}
 	}
 };
 class compute
@@ -57,16 +106,35 @@ public:
 		p2->storage();
 		p3->display();
 	}
+	~compute()
+	{
+		if (p1 != NULL)
+		{
+			delete p1;
+		}
+		if (p2 != NULL)
+		{
+			delete p2;
+		}
+		if (p3 != NULL)
+		{
+			delete p3;
+		}
+	}
 };
 int main()
 {
-	Cpu* p1 = new Intel;
-	Ram* p2 = new Ram1;
-	VideoCard* p3 = new VideoCard1;
-	compute p4(p1, p2, p3);
-	p4.test();
-	delete p1;
-	delete p2;
-	delete p3;
+	Cpu* p1 = new Intel("aaa");
+	Ram* p2 = new Ram1("bbb");
+	VideoCard* p3 = new VideoCard1("ccc");
+	compute* p4 = new compute(p1, p2, p3);
+	p4->test();
+	Cpu* a1 = new Intel("111");
+	Ram* a2 = new Ram1("222");
+	VideoCard* a3 = new VideoCard1("333");
+	compute* a4 = new compute(a1, a2, a3);
+	a4->test();
+	delete a4;
+	delete p4;
 	return 0;
 }
